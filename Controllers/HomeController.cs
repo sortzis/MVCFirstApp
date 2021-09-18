bd3ca32630500e202050f6af47e3ee4e2fd6a05d
sortzis <sortzis@mail.nmc.edu>
2021-09-17 20:38:39 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCFirstApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string ID)
+        {
+            string seeView = ID;
+            if (seeView == "cit")
+                return View("cit218");
+            else
+                return View();
+        }
+
+        //GET: Home
+        public string myIndex()
+        {
+            return "Sammi's Index";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
